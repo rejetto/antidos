d4763b6a88008e10ed7937484d417a17268ca38e
Massimo Melina <a@rejetto.com>
2025-02-16 00:34:56 +0100

    fix: requests are counted twice when "proxies" is enabled

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,12 +1,12 @@
-exports.version = 3
+exports.version = 3.1
 exports.description = "Ban IPs after too many requests in a short time. No persistence on restart."
 exports.apiRequired = 9.5 // newSocket
 exports.repo = "rejetto/antidos"
 
 exports.config = {
-    max: { type: 'number', min: 0, defaultValue: 500, helperText: "Max number of requests" },
+    max: { type: 'number', min: 0, defaultValue: 500, xs: 6, label: "Max requests" },
     seconds: { type: 'number', min: 1, defaultValue: 5, xs: 6, label: "Time window", unit: "seconds", helperText: "Limit in time" },
-    howLong: { type: 'number', min: 0, defaultValue: 0, xs: 6, unit: "seconds", helperText: "0 = infinite" },
+    howLong: { type: 'number', min: 0, defaultValue: 0, required: true, xs: 6, unit: "seconds", label: "Ban for", helperText: "0 = infinite" },
     whitelist: { type: 'string', multiline: true, helperText: "one ip per line; masks are supported" }
 }
 exports.configDialog = {
@@ -35,8 +35,8 @@ exports.init = api => {
         }
     }, 1000)
 
+    // early disconnection
     api.events.on('newSocket', ({ ip }) => {
-        if (isLocalHost(ip)) return
         if (isBanned(ip))
             return api.events.preventDefault
     })
@@ -46,31 +46,32 @@ exports.init = api => {
             clearInterval(timer)
         },
         async middleware(ctx) {
-            if (!api.getHfsConfig('proxies')) return // only with proxies the ip may be different from the one provided by the Socket
-            if (isLocalHost(ctx)) return
-            if (!isBanned(ctx.ip)) return
+            const {ip} = ctx
+            const res = isBanned(ip)
+            if (res === false) return
+            if (res === undefined) { // consider banning
+                let a = reqsByIp.get(ip)
+                if (!a)
+                    reqsByIp.set(ip, a = [])
+                a.push(Date.now())
+                if (a.length <= api.getConfig('max')) return
+                api.log("banning " + ip)
+                ban.add(ip)
+                reqsByIp.delete(ip)
+                const ms = api.getConfig('howLong') * 1000
+                if (ms) setTimeout(() => {
+                    ban.delete(ip)
+                    api.log("ban lifted " + ip)
+                }, ms)
+            }
             ctx.socket.destroy()
             return ctx.stop()
         }
     }
 
     function isBanned(ip) {
-        if (isWhiteListed(ip)) return
+        if (isLocalHost(ip) || isWhiteListed(ip)) return false
         if (ban.has(ip))
             return true
-        let a = reqsByIp.get(ip)
-        if (!a)
-            reqsByIp.set(ip, a = [])
-        a.push(Date.now())
-        if (a.length <= api.getConfig('max')) return
-        api.log("banning " + ip)
-        ban.add(ip)
-        reqsByIp.delete(ip)
-        const ms = api.getConfig('howLong') * 1000
-        if (ms) setTimeout(() => {
-            ban.delete(ip)
-            api.log("ban lifted " + ip)
-        }, ms)
-        return true
     }
 }
